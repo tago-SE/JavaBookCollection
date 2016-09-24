bd5bd42afc9767f60c9333e3e53c4cf356cf1926
tago-SE <tiago.redaelli@gmail.com>
2016-09-24 11:21:01 +0200

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,67 @@
+package lab_3b_1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Menu {
+    
+    private String header;
+    private ArrayList<String> options;
+    
+    public Menu(String header_) {
+        this.header = header_;
+        this.options = new ArrayList<>();
+    }
+
+    public void print() {
+        System.out.println(this.header);
+        Iterator<String> it = options.iterator();
+        int i = 0;
+        while (it.hasNext()) {
+            System.out.println(++i + ": " + it.next());
+        }
+    }
+    
+    public void addOption(String dialogue) {
+        this.options.add(dialogue);
+    }
+   
+    public int getUserOption() {
+        System.out.print("Option: ");
+        int option;
+        try {
+            option =  UserInput.getInt();
+        } catch (InvalidUserInputException ex) {
+            return this.getUserOption();
+        }
+        if (option <= 0 || option > this.options.size()) {
+            System.out.println("Outside bounds.");
+            return this.getUserOption();
+        }
+        return option;
+    }
+
+    /*
+        Useless methods I couldn't get rid of for some reason
+    */
+    
+    CollectionOfBooks addBooks() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    CollectionOfBooks removeBooks(CollectionOfBooks collection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    void listBooks(CollectionOfBooks collection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void searchBooks(CollectionOfBooks collection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void saveAndExit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
