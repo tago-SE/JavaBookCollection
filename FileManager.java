71d726360917289eb8ba0dc2aeede632a5a7b0f3
Beelzenord <fauzianordlund@Fauzias-MacBook-Air.local>
2016-09-25 23:02:22 +0200

    changes on interface

--- FileManager.java
+++ FileManager.java
@@ -1,46 +1,49 @@
+package lab3.Lab3_2;
 
-import java.io.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.io.Serializable;
+import lab_3b_1.Book;
 import lab_3b_1.CollectionOfBooks;
-    public class FileManager {
+
+    public class FileManager implements Serializable{
     
-    private final String fileName;
-    FileOutputStream merh = null;
-    ObjectOutputStream merhOut;
+    private String fileName;
+ 
     
-    FileInputStream finality = null;
-    ObjectInputStream finalityOis = null;
-    
-    CollectionOfBooks theCollection;
+    private CollectionOfBooks theCollection;
+   
+   
     public FileManager(String fileName_){
+        System.out.println("MERH");
         this.fileName = fileName_;
-        /*try{
-            merh = new FileOutputStream(fileName_);
-            merhOut = new ObjectOutputStream(merh);
-        }
-        catch(Exception e){
-            System.out.println("Unable to find file");
-        }
-        finally {
-            try {
-                if(merh!= null) merh.close();
-            }
-            catch(IOException e){}
-        }*/
+      
     }
-  
     
-    void connectToBooks(CollectionOfBooks newCollections){
-        theCollection = newCollections;
-    }
-    public CollectionOfBooks load(){
+    
+   // public CollectionOfBooks load(){
+       /* System.out.println("Load");
         try{
             finality = new FileInputStream(fileName);
+            System.out.println("Executed 1");
             finalityOis = new ObjectInputStream(finality); 
-            theCollection = (CollectionOfBooks) finalityOis.readObject();
+            System.out.println("Executed 2");
+            CollectionOfBooks cob = (CollectionOfBooks) finalityOis.readObject();
+            System.out.println("Executed 3");
+            System.out.print(cob.toString());
+            
         } 
         catch(IOException e){
-            System.out.println(e);
+            System.out.println(e+ "This type");
         }
         catch(ClassNotFoundException e){
             System.out.println("Negative Colonel");
@@ -51,20 +54,29 @@ import lab_3b_1.CollectionOfBooks;
             }
         catch(IOException e){};
         return null;    // Throw exception if failed to load.
-     }
-    }
+     }*/
+   // }
     public void save(CollectionOfBooks toSave) {
-        try{
-            merhOut.writeObject(toSave);
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-         finally {
-            try {
-                if(merh!= null) merh.close();
-            }
-            catch(IOException e){}
-        }
-    }
+        System.out.println("Saving");  
+        ArrayList<Book> book = toSave.getBooks();
+        FileOutputStream fout = null;
+        
+        try {
+	      fout = new FileOutputStream(fileName);
+	      ObjectOutputStream ous = new ObjectOutputStream(fout);
+	      System.out.println("Up to here");
+	      ous.writeObject(book);
+	      
+	      System.out.println("Serializing successfully completed");
+	    }
+	    catch (Exception e) {
+	      System.out.println(e);
+	    }
+	    finally {
+	    	try {
+	    		if(fout != null) fout.close();
+	    	}
+	    	catch(IOException e) {}
+	    }
+	  }
 }
