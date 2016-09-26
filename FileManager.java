06068537de9fef7baef783cc8acadf90ac56c23c
Beelzenord <alnor@kth.se>
2016-09-26 19:11:14 +0200

    Update FileManager.java
    with comments

--- FileManager.java
+++ FileManager.java
@@ -1,21 +1,45 @@
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import lab_3b_1.CollectionOfBooks;
+
+/**
+ * An instance of this class represents a collection of books. With
+ * 
+ * @author Tiago Redaelli <tiagor@kth.se> and Alexander Nordlund.
+ */
 public class FileManager {
     private String fileName;
-     FileOutputStream fout = null;
-     public FileManager(){
-        this.fileName = "N.ser";
+    private FileOutputStream fout = null;
+     
+     
+     public FileManager(String fileName_){
+        /**
+          * Contructor of FileManger object. uses a string to name
+          * the serial file to be used.
+          */
+         this.fileName = fileName_;
+        
+        
      }
-    
+          /**
+           * @param collection all books we added that we are going to save
+           * to file.
+           * @throws Exeception
+           * @throws IOException
+           * @serialData collection (of books), outputs object to serial file.
+           */
+         
      public void saveToFile(CollectionOfBooks collection){
-         ArrayList<Book> books = collection.getBooks();
+
          try {
-	      fout = new FileOutputStream("N.ser");
+	      fout = new FileOutputStream(fileName);
 	      ObjectOutputStream ous = new ObjectOutputStream(fout);
 	      
-	      ous.writeObject(books);
+	      ous.writeObject(collection);
 	      
 	      System.out.println("Serializing successfully completed");
 	    }
@@ -27,25 +51,33 @@ public class FileManager {
 	    		if(fout != null) fout.close();
 	    	}
 	    	catch(IOException e) {}
-	    }
+	    } 
+         
      }
-     public void loadFromFile(){
+         /**
+           * @param collection all books we added that we are going to save
+           * to file.
+           * @throws ClassNotFoundException
+           * @throws IOException
+           * @serialData collection (of books), inputs object from serial file.
+           */
+         
+     public CollectionOfBooks loadFromFile(){
          FileInputStream fin = null;
+         CollectionOfBooks theCollection = new CollectionOfBooks();
          try{
-             fin = new FileInputStream("N.ser");
+             fin = new FileInputStream(fileName);
              ObjectInputStream ois = new ObjectInputStream(fin);
-             ArrayList<Book> book = (ArrayList<Book>) ois.readObject();
+             theCollection = (CollectionOfBooks) ois.readObject();
              System.out.println("Deserializing");
              
-             for(Book b : book){
-                 System.out.println(b.toString());
-             }
+         
          }
          catch(IOException e){
              System.out.println(e);
          }
          catch(ClassNotFoundException e){
-             System.out.println("MERH");
+             System.out.println(e);
          }
          finally {
              try {
@@ -53,6 +85,7 @@ public class FileManager {
              }
              catch(IOException e){}
          }
+         return theCollection;
      }
 }
 
