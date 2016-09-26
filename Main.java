5e2a318d441d5e5d1192e3a1c022c54f9f552151
tago-SE <tiago.redaelli@gmail.com>
2016-09-26 17:17:14 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,16 +6,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws NoOptionException  {
-         public static void main(String[] args)throws NoOptionException,IOException {	  	
-        FileManager filemanager = new FileManager();
-        CollectionOfBooks collection = filemanager.loadFromFile();	
-        UserInterface ui = new UserInterface(collection);
         
-       // ui.hookUItoFileManager(filemanager);
-        while(ui.runMainMenu()){
-            //
-        }
-        filemanager.saveToFile(collection);
-        collection = ui.getUserChanges();
+        public static void main(String[] args) throws NoOptionException, IOException {	  	
+           
+            FileManager filemanager = new FileManager();
+            
+            // Loads the book collcetion from 
+            CollectionOfBooks collection = filemanager.loadFromFile();	
+            UserInterface ui = new UserInterface(collection);
+
+           // ui.hookUItoFileManager(filemanager);
+            while(ui.runMainMenu()){
+                //
+            }
+            filemanager.saveToFile(collection);
+            collection = ui.getUserChanges();
      }
 }
