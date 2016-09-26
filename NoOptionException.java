40c8546e7261c098613af0775734575daf9f2c90
tago-SE <tiago.redaelli@gmail.com>
2016-09-26 19:12:56 +0200

    Update NoOptionException.java

--- NoOptionException.java
+++ NoOptionException.java
@@ -1,5 +1,17 @@
 package lab_3b_1;
 
+/**
+ * NoOptionException is an object that takes 
+ * 
+ * @author Tiago Redaelli and Alexander Nordlund
+ * @version 1.2
+ * @since 1.0
+ */
+
+/** 
+ * Creates an exception that tells the program it failed to get a valid user option. 
+ * */
+
 public class NoOptionException extends Exception {
     public NoOptionException() {
         super("Failed to find a user option.");
