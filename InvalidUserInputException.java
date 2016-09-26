65f24fd296b45eae18cf8465b95de1e223afa477
tago-SE <tiago.redaelli@gmail.com>
2016-09-26 19:14:35 +0200

    Update InvalidUserInputException.java

--- InvalidUserInputException.java
+++ InvalidUserInputException.java
@@ -1,11 +1,27 @@
 package lab_3b_1;
 
+/**
+ * InvalidUserInputException is an object which extends Exceptions 
+ * to tell the program when it failed to recieve  a proper input from the user. 
+ * 
+ * @author Tiago Redaelli and Alexander Nordlund
+ * @version 1.2
+ * @since 1.0
+ */
+
+/** 
+ * Creates a generic user input exception failure. 
+ * */
 class InvalidUserInputException extends Exception {
     public InvalidUserInputException() {
         super("Invalid user input.");
     }
     
+    /** Creates a specific user input exception failure.
+     * @param input is the text recieved  from the user. 
+    * */
     public InvalidUserInputException(String input) {
         super("Invalid user input: " + input);
     }
 }
+
