9e3f341418356b59dbdbf9cc6cb22f5225aeb221
Beelzenord <alnor@kth.se>
2016-09-26 17:00:14 +0200

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,52 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author fauzianordlund
+ */
+public class Circle extends FillableShape{
+    private double diameter;
+
+    public Circle(double x, double y, Color color, boolean filled,double diameter) {
+        super(x, y, color, filled);
+        this.diameter=diameter;
+    }
+     public Circle(double x, double y, Color color, boolean filled) {
+        super(x, y, color, filled);
+    }
+     public double getDiameter(){
+         return diameter;
+     }
+     public void setDiameter(double diameter){
+         this.diameter=diameter;
+     }
+    @Override
+    public void paint(GraphicsContext gc) {
+       gc.setFill(super.getColor());
+       gc.setStroke(super.getColor());
+        if(super.isFilled()){
+           gc.fillOval(super.getX(),super.getY(),diameter/2, diameter/2);
+        }
+        else{
+           gc.strokeOval(super.getX(),super.getY(),diameter/2, diameter/2);
+        }
+    }
+    @Override
+    public void constrain(
+            double CircleX, double CircleY, 
+            double boxWidth, double boxHeight) {
+        // If outside the box - calculate new dx and dy
+        if (CircleX < 0) {
+            super.setVelocity(Math.abs(super.getDx()), super.getDy());
+        } else if (CircleX + (diameter / 2) > boxWidth) {
+            super.setVelocity(-Math.abs(super.getDx()), super.getDy());
+        }
+        if (CircleY < 0) {
+            super.setVelocity(super.getDx(), Math.abs(super.getDy()));
+        } else if (CircleY +(diameter / 2)> boxHeight) {
+            super.setVelocity(super.getDx(), -Math.abs(super.getDy()));
+        }
+    }
+    
+}
