73a14b3af5e3db518cb28e358ed4bd37ef962f3f
tago-SE <tiago.redaelli@gmail.com>
2016-09-26 20:39:15 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package lab_3b_1;
+import java.io.IOException;
 
 public class Main {
 
@@ -6,27 +7,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws NoOptionException  {
-        
-        public static void main(String[] args) throws NoOptionException, IOException {	  	
-           
-             // Creates the file manager with a serialization file to read from.
-            FileManager filemanager = new FileManager("file.ser");     
-            
-            // Deserialize (load) a collection of books from the file manager or returns an empty collection.
-            CollectionOfBooks collection = filemanager.loadFromFile();	
-            
-            // Creates the user interface and sends the collection of books over to be handled internaly until finished. 
-            UserInterface ui = new UserInterface(collection);
+        // Creates the file manager with a serialization file to read from.
+        FileManager filemanager = new FileManager("file.ser");     
 
-            // Run the program until user commands exit.
-            while(ui.runMainMenu()){
+        // Deserialize (load) a collection of books from the file manager or returns an empty collection.
+        CollectionOfBooks collection = filemanager.loadFromFile();
+
+        
+        // Creates the user interface and sends the collection of books over to be handled internaly until finished. 
+        UserInterface ui = new UserInterface(collection);
+
+        // Run the program until user commands exit.
+        while(ui.runMainMenu()){
                 // Empty
-            }
+        }
             
-            // Gets all the changes to the menu from user interface.
-            collection = ui.getUserChanges();
+        // Gets all the changes to the menu from user interface.
+        collection = ui.getUserChanges();
             
-            // Saves the collection of books. 
-            filemanager.saveToFile(collection);
+        // Saves the collection of books. 
+        filemanager.saveToFile(collection);
      }
 }
